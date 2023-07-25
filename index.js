3664bbbb03ddd5b7e882f3d0692ff388e7d23a89
wachirarobbinson <ngirigacharobbinson@gmail.com>
2023-07-25 09:00:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.body.remove('main#main')
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Robbinson is the champion'
